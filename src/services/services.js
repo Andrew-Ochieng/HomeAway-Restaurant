9b04f596828108d6f320aca0de5e194023c0d91e
rodgersochieng <rodger.ochieng@student.moringaschool.com>
2022-09-28 21:56:09 +0300

    editing conference  section

--- src/services/services.js
+++ src/services/services.js
@@ -1,6 +1,7 @@
 import React from "react";
 import gymImage from "./service-images/gym.jpg";
 import spaImage from "./service-images/spa.jpg";
+import conImage from "./service-images/conference.jpg";
 export default function Services() {
   return (
     <>
@@ -18,7 +19,17 @@ export default function Services() {
         <img className="spaImg" src={spaImage} alt="spapic" />
         <div className="spaMessage">
           <span> SPA AND MASSAGE SERVICES</span>
+          <h2>welcome to a world class SPA and Massage services</h2>
+          <p>Where Luxury Meets Qualit</p>
         </div>
+      </div>
+      <div className="conference">
+        <div className="conferenceMsg">
+          <span>CONFERENCE AND CORPORATE ROOMS</span>
+          <h2>Welcome to a world class conference rooms</h2>
+          <p>We give you the best Meeting environment</p>
+        </div>
+        <img className="conferenceImg" src={conImage} alt="conferencepic" />
       </div>
     </>
   );
