49f04f4123755e4d2fdf52ff4d18b79888bed681
Andrew-Ochieng <andrewomosh44@gmail.com>
2022-09-28 16:07:17 +0300

    Add routing configurations

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+import AboutUs from './pages/AboutUs';
+import ContactUs from './pages/ContactUs';
+import LandingPage from './pages/LandingPage';
+import Menu from './pages/Menu';
+import Services from './pages/Services';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+
+        <Routes>
+          <Route path='/' element={ <LandingPage />} />
+          <Route path='/about' element={ <AboutUs />} />
+          <Route path='/menu' element={ <Menu />} />
+          <Route path='/services' element={ <Services />} />
+          <Route path='/contactus' element={ <ContactUs />} />
+        </Routes>
+
+        <Footer />
+      </Router>
     </div>
   );
 }
