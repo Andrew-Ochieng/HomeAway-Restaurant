8f3d6a5c8816c533a343b23c4301a4829e12bd1b
rodgersochieng <rodger.ochieng@student.moringaschool.com>
2022-09-28 19:26:08 +0300

    connecting directories

---
+++ src/services/services.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Services() {
+  return (
+    <>
+      <div className="gym">
+        <h2>Welcome to our world class fitness services</h2>
+        <p>we give you the best there is in fitness</p>
+      </div>
+    </>
+  );
+}
