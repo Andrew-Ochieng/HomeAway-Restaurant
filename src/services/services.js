28217bdb78deb614fb5e2a79bd85c3422447d66b
rodgersochieng <rodger.ochieng@student.moringaschool.com>
2022-09-28 21:13:04 +0300

    added basic texts

--- src/services/services.js
+++ src/services/services.js
@@ -1,12 +1,16 @@
 import React from "react";
+import gymImage from "./service-images/gym.jpg";
 export default function Services() {
   return (
-    <>
+    <section className="gymsection">
       <div className="gym">
+        <span>GYM $ FITNESS SERVICES </span>
         <h2>Welcome to our world class fitness services</h2>
         <p>we give you the best there is in fitness</p>
       </div>
-      <div className="gym-image">{/* <img src={}alt="gym" /> */}</div>
-    </>
+      <div className="fitness">
+        <img src={gymImage} alt="gympic" height="600" width="900" />
+      </div>
+    </section>
   );
 }
