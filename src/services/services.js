29c4d15ec861ed1be69f60ea5c6d463c9742e68a
rodgersochieng <rodger.ochieng@student.moringaschool.com>
2022-09-28 22:12:11 +0300

    editing pool  section

--- src/services/services.js
+++ src/services/services.js
@@ -2,6 +2,8 @@ import React from "react";
 import gymImage from "./service-images/gym.jpg";
 import spaImage from "./service-images/spa.jpg";
 import conImage from "./service-images/conference.jpg";
+import poolImg from "./service-images/pool.jpg";
+
 export default function Services() {
   return (
     <>
@@ -30,6 +32,15 @@ export default function Services() {
           <p>We give you the best Meeting environment</p>
         </div>
         <img className="conferenceImg" src={conImage} alt="conferencepic" />
+      </div>
+
+      <div className="pool">
+        <img className="poolImg" src={poolImg} alt="poolimage" />
+        <div className="poolMsg">
+          <span>POOL AND JACUZZI SERVICES</span>
+          <h2>Welcome to the world's most refreshing experience </h2>
+          <p>we offer the best of jacuzzi and pool experiences</p>
+        </div>
       </div>
     </>
   );
