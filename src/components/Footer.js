8adb050060a1685abdade0ddbfa35ae31d76cfaa
Andrew-Ochieng <andrewomosh44@gmail.com>
2022-09-29 18:30:36 +0300

    add footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,53 @@
 
 const Footer = () => {
     return ( 
-        <div>
-            
+        <div className="bg-[#959E61] md:px-16 px-8 md:py-16 py-8">
+            <div className="md:flex md:justify-between justify-between items-center">
+                <div className="md:w-1/4 w-full">
+                    <h1 className="md:text-2xl text-xl font-semibold">Home-Away</h1>
+                    <p className="my-2">
+                        This is a type of restaurant which typically serves food and drinks, in 
+                        addition to light refreshments from baked foods or snacks.
+                    </p>
+                    <div className="flex  md:text-2xl text-lg text-gray-800">
+                        <i class="hover:text-white mr-2 fa fa-whatsapp"></i>
+                        <i class="hover:text-white mx-2 fa fa-facebook"></i>
+                        <i class="hover:text-white mx-2 fa fa-instagram"></i>
+                        <i class="hover:text-white mx-2 fa fa-linkedin"></i>
+                    </div>
+                </div>
+
+                <div>
+                    <h1 className="md:text-xl text-lg text-white font-medium">Services</h1>
+                    <div>
+                        <p>Online-Order</p>
+                        <p>Pre-preservation</p>
+                        <p>24/7 Services</p>
+                        <p>Foodie Place</p>
+                        <p>Super Chefs</p>
+                    </div>
+                </div>
+                <div>
+                    <h1 className="md:text-xl text-lg text-white font-medium">Quick Links</h1>
+                    <div>
+                        <p>Menu</p>
+                        <p>Reviews</p>
+                        <p>Blogs</p>
+                        <p>Reservable Foods</p>
+                        <p>Order Foods</p>
+                    </div>
+                </div>
+                <div>
+                    <h1 className="md:text-xl text-lg text-white font-medium">Help</h1>
+                    <div>
+                        <p>Contact</p>
+                        <p>Support</p>
+                        <p>FAQ</p>
+                    </div>
+                </div>
+
+                
+            </div>
         </div>
      );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,33 @@
 import { Link } from 'react-router-dom'
-
+import { useState } from 'react';
+// #61959E && #959E61
 const Navbar = () => {
-    // colors
-    // #61959E && #959E61
+    // toggle navbar
+    const [menu, setMenu] = useState("")
+    
+    function showMenu() {
+        
+    }
+
 
     return ( 
         <div>
             <nav className='bg-[#959E61]'>
-                <div className='flex justify-around items-center p-6'>
-                    <div>
-                        <Link to='/' className='md:text-xl text-lg font-semibold'>Home-Away</Link>
+                <div className='flex justify-between items-center p-6'>
+                    <div className='flex justify-between items-center'>
+                        <Link to='/' className='md:text-2xl text-xl font-semibold'>Home-Away</Link>
+                    
+                        <button id="hambuger" class="md:hidden text-2xl">
+                            <i onclick={showMenu} class="fa fa-bars cursor-pointer"></i>
+                        </button>
+
+                        <button id="times" class="text-2xl hidden">
+                            <i onclick="hideMenu()" class="fa fa-times cursor-pointer"></i>
+                        </button>
                     </div>
 
                     <div>
-                        <ul className='flex justify-center items-center text-lg'>
+                    <ul id="menu" class="md:flex hidden items-center text-[#1a2238] font-semibold md:text-xl text-lg">
                             <li className='mx-4'>
                                 <Link to='/'>Home</Link>
                             </li>
