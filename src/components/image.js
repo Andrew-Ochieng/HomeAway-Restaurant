090cc570cd8f1111c4dfa4ad5d5e62bb0172d498
Andrew-Ochieng <andrewomosh44@gmail.com>
2022-09-30 04:49:45 +0300

    add about-page

--- src/components/image.js
+++ src/components/image.js
@@ -3,7 +3,7 @@ import React from "react";
 function Image() {
     return (
       <div>
-        <img src="https://images.pexels.com/photos/2977515/pexels-photo-2977515.jpeg?cs=srgb&dl=pexels-rene-asmussen-2977515.jpg&fm=jpg" alt="chefs"/>
+        <img className="rounded-2xl" src="https://images.pexels.com/photos/2977515/pexels-photo-2977515.jpeg?cs=srgb&dl=pexels-rene-asmussen-2977515.jpg&fm=jpg" alt="chefs"/>
       </div>
     );
   }
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -4,21 +4,26 @@ import Customerpic from "../components/customersPIc";
 
 const AboutUs = () => {
     
-    function search() {
-        alert('Searching...');
+    function Search() {
+        console.log('Searching...');
     }
 
     return ( 
-        <div>
-            <h1>We serve the test you love</h1>
+        <div className="lg:mx-32 md:mx-16 mx-8 md:my-16 my-8">
+            <div className="sm:flex ">
+                <div>
+                    <h1 className="md:text-4xl text-2xl">We serve the test you love</h1>
+                    <p>We serve the test of restuarant which typically serves foods and drinks in addition to light refreshners such as baked goods or snack.</p>
+                    <input type="search" id="mySearch" name="q" />
+                    <button onClick={Search}>
+                        Search
+                    </button>
+                </div>
+                <div className="lg:w-2/3 md:w-3/4 w-full">
+                    <Image/>
+                </div>
+            </div>
 
-            <p>We serve the test of restuarant which typically serves foods and drinks in addition to light refreshners such as baked goods or snack.</p>
-
-            <input type="search" id="mySearch" name="q" />
-
-            <button onClick={search}>
-             search
-             </button>
              <h2>Our chefs</h2>
              <Image/>
 
